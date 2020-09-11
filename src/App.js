a1fbb320b9aa2a20e1eccb158de98b74e1596052
meganthadev <meganthadev@gmail.com>
2020-09-10 22:59:25 -0700

    Import redux/thunk/prov

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
+
+//* componentDidMount() {
+//*     fetch('http://localhost:3000/api/v1/products')
+//*     .then(resp => resp.json())
+//*      .then(data => console.log(data))
+//*  } 
   render () {
   return (
     <div className="App">
@@ -9,5 +15,7 @@ class App extends React.Component {
   );
   }
 }
+
+
 
 export default App;
