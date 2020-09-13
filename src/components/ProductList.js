635d69c0c9f8ef6fc082b05734cfac77634d71cc
meganthadev <meganthadev@gmail.com>
2020-09-13 01:50:52 -0700

    Map Products in List

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -4,7 +4,7 @@ const ProductList = (props) => {
 
     return (
         <div>
-            {props.products.map(product => <li key={product.id}>{product.name} - {product.image_url} - {product.rating}</li>
+            {props.products.map(product => <li key={product.id}>{product.name} -- {product.image_url} -- {product.rating}</li>
             )}
         </div>
     )
