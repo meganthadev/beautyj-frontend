afe083662f7a29064c54524d98e2708678abf3b6
meganthadev <meganthadev@gmail.com>
2020-09-13 01:52:15 -0700

    Add add prod case to reducer

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -3,6 +3,8 @@ export default function productReducer(state = { products: [] }, action) {
     switch (action.type) {
         case 'FETCH_PRODUCTS':
             return { products: action.payload }
+        case 'ADD_PRODUCT':
+            return {...state, accounts: [...state.products, action.paylod]}
         default:
             return state
     }
