67fc17f5820116598c53a47ac3fcb5c1e1a345ac
meganthadev <meganthadev@gmail.com>
2020-09-12 23:06:29 -0700

    Add gete prods action

--- src/actions/fetchProducts.js
+++ src/actions/fetchProducts.js
@@ -1,6 +1,11 @@
 
-export function fetchProducts(action) {
-
-    return action
-
+export function fetchProducts() {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/products')
+      .then(resp => resp.json())
+      .then(products => dispatch({
+        type: 'FETCH_PRODUCTS',
+        payload: products
+      }))
+  }
 }
