9e6637f2afa3b02a84f74a062fdb683e14853b50
meganthadev <meganthadev@gmail.com>
2020-09-12 17:51:15 -0700

    Create store/composeenhancers

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
+import productReducer from './reducers/productReducer';
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 
-let store = createStore(reducer, applyMiddleware(thunk))
+let store = createStore(productReducer, composeEnhancers(applyMiddleware(thunk)))
 
 
 ReactDOM.render(
-<Provider>
+ <Provider store={store}>
     <App />
-</Provider>,
+ </Provider>,
  document.getElementById('root'));
