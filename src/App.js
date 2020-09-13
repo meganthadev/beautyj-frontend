defe87d65e9f98584a6d637e9397122ab11eed5e
meganthadev <meganthadev@gmail.com>
2020-09-12 23:05:58 -0700

    Add components/props to App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {fetchProducts} from './actions/fetchProducts';
+import ProductsContainer from './containers/ProductsContainer'
 
 class App extends React.Component {
 
- componentDidMount() {
-  this.props.fetchProducts({type: 'FETCH_PRODUCTS', payload: {name: 'Water'}})
- } 
-
-
-  render () {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <ProductsContainer />
+      </div>
+    );
   }
 }
 
-// const mapStateToProps = (state) => {
-//  return {
-//    products: state.products
-//  }
-// }
 
 
-export default connect(null, {fetchProducts})( App);
+export default App;
