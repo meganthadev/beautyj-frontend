b4d90b2233074cb9e7baf51aaa9d2ae49acaf333
meganthadev <meganthadev@gmail.com>
2020-09-13 21:39:43 -0700

    Create Details comp

---
+++ src/components/Details.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DetailContainer from '../containers/DetailContainer';
+
+
+const Details = (props) => {
+
+        return(
+            <div>
+                Details
+            </div>
+
+        )
+
+}
+export default Details;
