b35a8eac2f047176431bc611ca8cdc4fbddb9a3a
meganthadev <meganthadev@gmail.com>
2020-09-14 02:36:40 -0700

    Add props to det contain

--- src/containers/DetailContainer.js
+++ src/containers/DetailContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import DetailForm from '../components/DetailForm';
-import Details from '../components/Details';
+import DetailForm from '../components/DetailForm'
+import Details from '../components/Details'
 
 
 
@@ -9,13 +9,11 @@ class DetailContainer extends React.Component {
     render() {
         return (
             <div>
-               <DetailForm />
-               <Details detail={this.props.product && this.props.product.detail}/>
+               <DetailForm product={this.props.product} /><br></br>
+               <Details detail={this.props.product && this.props.product.detail} />
             </div>
         )
-    }
 
-
+ }
 }
-
-export default DetailContainer;
+export default DetailContainer
