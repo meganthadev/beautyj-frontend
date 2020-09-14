16b02327f6af7608cf7ef7c7b70d1bf218b81d64
meganthadev <meganthadev@gmail.com>
2020-09-13 21:40:24 -0700

    Create DetContainer with Prod props

---
+++ src/containers/DetailContainer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import DetailForm from '../components/DetailForm';
+import Details from '../components/Details';
+
+
+
+class DetailContainer extends React.Component {
+
+    render() {
+        return (
+            <div>
+               <DetailForm />
+               <Details detail={this.props.product && this.props.product.detail}/>
+            </div>
+        )
+    }
+
+
+}
+
+export default DetailContainer;
