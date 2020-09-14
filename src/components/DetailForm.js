0a34cd18b742d495a4254a799581c5d4d2063020
meganthadev <meganthadev@gmail.com>
2020-09-13 21:38:41 -0700

    Create Detail Form skeleton with props

---
+++ src/components/DetailForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class DetailForm extends React.Component {
+
+    render() {
+        return(
+            <div>
+                Detail Form
+            </div>
+      )
+    }
+
+
+}
+
+export default DetailForm;
