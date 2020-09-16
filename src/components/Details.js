51bc1cc05725bd5df62d40af9432abffbeccd8b1
meganthadev <meganthadev@gmail.com>
2020-09-16 11:26:01 -0700

    Add detail props to page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-const Details = (props) => {
 
-     return (
-        <div>
-            {props.details && props.details.map(detail =>
-                <li key={detail.id}> {detail.description} </li>
-            )}
-        </div>
-    )
+const Details = (props) => {
+   return (
+      <div>
+         <h3>Product Details: </h3>
+         <p>{props.detail && props.detail.description}</p>
+         <h4>Your Review: </h4>
+         <p>{props.detail && props.detail.full_review}</p>
+         <h4>Ingredients: </h4>
+         <p>{props.detail && props.detail.ingredients}</p>
+      </div>
+   )
 }
 
 export default Details
